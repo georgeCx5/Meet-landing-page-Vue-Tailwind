0864f22b78f45fdb8253f7d01e183739a5424ce7
georgeCx5 <69874793+georgeCx5@users.noreply.github.com>
2023-06-29 01:13:57 -0500

    Add files via upload
    - Mobile ✅
    - Tablet ✅
    - Desktop ⌛

--- App.vue
+++ App.vue
@@ -12,6 +12,7 @@ export default {
       coverImgLeft: 'bg-[url(@/assets/images/desktop/image-hero-left.png)]',
       coverImgRight: 'bg-[url(@/assets/images/desktop/image-hero-right.png)]',
       imgFooterMB: 'bg-[url(@/assets/images/mobile/image-footer.jpg)]',
+      imgFooterTB: 'TB:bg-[url(@/assets/images/tablet/image-footer.jpg)]',
     }
   },
   components: {
@@ -25,45 +26,52 @@ export default {
 </script>
 <template>
   <body class=" flex flex-col items-center bg-neo-white font-red-hat-display overflow-hidden">
-    <header class=" flex flex-col items-center w-full max-w-[425px] pt-12 pb-16">
-      <Logo class=" mb-[58px]" />
-      <div :class="` w-[414px] h-[153px] mb-12 ${coverImgMB} bg-cover`"></div>
-      <div class=" flex flex-col gap-6 mb-8 px-6 text-center">
-        <h1 class=" text-neo-dark-gray text-[40px] leading-[40px] font-black">
+    <header class=" flex flex-col items-center w-full max-w-[425px] TB:max-w-[1024px] pt-12 pb-16">
+      <Logo class=" mb-[58px] TB:mb-16" />
+      <div :class="` w-[414px] TB:w-[820px] h-[153px] TB:h-[303px] mb-12 TB:mb-[72px] ${coverImgMB} bg-cover`"></div>
+      <div class=" flex flex-col gap-6 mb-8 px-6 TB:px-[155px] text-center">
+        <h1 class=" text-neo-dark-gray text-[40px] TB:text-[48px] leading-[40px] TB:leading-[48px] font-black">
           Group Chat for Everyone
         </h1>
         <p class=" text-neo-gray text-[16px] leading-[26px] font-medium">
           Meet makes it easy to connect with others face-to-face virtually and collaborate across any device.
         </p>
       </div>
-      <div class=" flex flex-col items-center gap-4">
+      <div class=" flex flex-col TB:flex-row items-center gap-4">
         <HeaderBtn :data-index="0" btn-text="Download" btn-span-text="v1.3" />
         <HeaderBtn :data-index="1" btn-text="What is it?" w-size="w-[139px]" />
       </div>
     </header>
-    <main class=" flex flex-col gap-16 w-full max-w-[425px] px-6">
-      <RoundedArrow :text-number="1" />
-      <div class=" grid grid-cols-2 gap-6">
+    <main class=" flex flex-col gap-16 TB:gap-0 w-full max-w-[425px] TB:max-w-[1024px] px-6 TB:px-10">
+      <RoundedArrow class=" TB:mb-[68px]" :text-number="1" />
+      <div class=" grid grid-cols-2 TB:grid-cols-4 gap-6 TB:mb-12">
         <GridImages :img-index="0" />
         <GridImages :img-index="1" />
         <GridImages :img-index="2" />
         <GridImages :img-index="3" />
       </div>
-      <div class="  text-[16px] leading-[26px] text-center">
-        <h3 class=" mb-6 text-neo-cyan tracking-[4px] font-black uppercase">Built for modern use</h3>
-        <h2 class=" mb-8 text-neo-dark-gray text-[32px] leading-[36px] font-black">Smarter meetings, all in one place</h2>
+      <div class=" TB:mb-20 TB:px-14 text-[16px] leading-[26px] text-center">
+        <h3 class=" mb-6 TB:mb-4 text-neo-cyan tracking-[4px] font-black uppercase">
+          Built for modern use</h3>
+        <h2
+          class=" mb-8 TB:px-16 text-neo-dark-gray text-[32px] TB:text-[40px] leading-[36px] TB:leading-[44px] font-black">
+          Smarter meetings, all in one place</h2>
         <p class=" text-neo-gray font-medium">
           Send messages, share files, show your screen, and record your meetings — all in one
           workspace. Control who can join with invite-only team access, data encryption, and data export.
         </p>
       </div>
-      <RoundedArrow :text-number="2" />
+      <RoundedArrow class=" mb-[-28px]" :text-number="2" />
     </main>
-    <footer :class="` relative w-full max-w-[425px] mt-[-28px] pt-[92px] pb-[72px] bg-neo-cyan text-neo-white`">
-      <div :class="` absolute top-0 w-full h-full ${imgFooterMB} bg-cover opacity-10 pointer-events-none`"></div>
-      <div class=" relative flex flex-col items-center px-6 text-center z-10">
-        <h2 class=" mb-6 text-[32px] leading-[36px] font-black">Experience more together</h2>
-        <p class=" mb-8 text-[18px] leading-[26px] font-medium">
+    <footer
+      :class="` relative w-full max-w-[425px] TB:max-w-[1024px] pt-[92px] pb-[72px] TB:pb-[66px] bg-neo-cyan text-neo-white`">
+      <div :class="` absolute top-0 w-full h-full ${imgFooterMB} ${imgFooterTB} bg-cover opacity-10 pointer-events-none`">
+      </div>
+      <div class=" relative flex flex-col items-center px-6 TB:px-24 text-center z-10">
+        <h2 class=" mb-6 TB:mb-8 TB:px-8 text-[32px] TB:text-[40px] leading-[36px] TB:leading-[44px] font-black">
+          Experience more together
+        </h2>
+        <p class=" mb-8 TB:mb-10 text-[18px] leading-[26px] font-medium">
           Stay connected with reliable HD meetings and unlimited one-on-one and group video sessions.
         </p>
         <HeaderBtn :data-index="1" btn-text="Download" btn-span-text="v1.3" />
